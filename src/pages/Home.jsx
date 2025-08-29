050b738fadafde3907f5181a2ec6384e2e543631
wendoughlee <wvr0312@gmail.com>
2025-08-29 15:10:26 -0500

    added footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -29,5 +30,6 @@ export const Home = () => {
         </main>
 
         {/* Footer */}
+        <Footer />
     </div>
 };
